f6fbd70f31d6c43d236d48b900acfb4eaccf9f12
jbeemster <joshua.a.beemster@gmail.com>
2024-01-17 11:53:26 +0100

    Bump service module to v0.2.1 (closes #34)

--- main.tf
+++ main.tf
@@ -390,7 +390,7 @@ locals {
 
 module "service" {
   source  = "snowplow-devops/service-ec2/aws"
-  version = "0.2.0"
+  version = "0.2.1"
 
   user_supplied_script = local.user_data
   name                 = var.name
